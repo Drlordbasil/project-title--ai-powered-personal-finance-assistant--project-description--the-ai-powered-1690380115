c327674bba9dec805bfc3e9a4017fecc7480d690
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-10 13:40:44 -0400

    Auto-formatting code

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ class Expense:
         self.amount = amount
         self.category = category
 
+
 class Budget:
     def __init__(self, category, amount):
         self.category = category
         self.amount = amount
 
+
 class Goal:
     def __init__(self, name, amount):
         self.name = name
         self.amount = amount
+
 
 class PersonalFinanceAssistant:
     def __init__(self):
@@ -54,7 +57,8 @@ class PersonalFinanceAssistant:
 
     def monitor_budgets(self):
         for category, budget in self.budgets.items():
-            expenses_in_category = sum(expense.amount for expense in self.expenses if expense.category == category)
+            expenses_in_category = sum(
+                expense.amount for expense in self.expenses if expense.category == category)
             if expenses_in_category > budget.amount:
                 print("Budget exceeded for", category)
             elif expenses_in_category >= budget.amount * 0.8:
@@ -81,6 +85,7 @@ class PersonalFinanceAssistant:
     def ensure_security_and_privacy(self):
         # Implement security and privacy measures for data encryption and compliance with regulations
         pass
+
 
 if __name__ == "__main__":
     pfa = PersonalFinanceAssistant()
